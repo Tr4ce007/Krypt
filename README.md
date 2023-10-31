c3e03f57d249742435eca2d09844e86e8f0ce510
Tr4ce007 <YourEmail@Company.com>
2023-10-31 12:02:32 +0530

    init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Krypt App
+Send Ethereum to others from our website.
+<br>
+- Check it out here @ [Krypt](https://tr4ce007.github.io/Krypt/)
---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+
+import abi from './Transactions.json';
+
+export const contractABI = abi.abi;
+export const contractAddress = '0xdfccE690a0D5a907E75D4959284251775727fE13';
